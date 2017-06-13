1a0e8369d2acafe9de516b759b8dbcabd829f722
Tina Holly <tinacious.design@gmail.com>
2017-06-12 20:03:30 -0400

    bootstrap currency conversion app

---
+++ currency.js
@@ -0,0 +1,17 @@
+$(function () {
+  /*
+
+  make a get request to the following: 
+    https://api.fixer.io/latest?base=CAD
+
+    On form submit, do the conversion and display the converted amount to the user
+  */
+
+  $.get()
+    .done(function (res) {
+      //
+    })
+    .fail(function (err) {
+      //
+    });
+});
